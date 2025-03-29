ff6de9c03e9deaa101a4e57df118d7535db4d80e
DaniSick <dinoretz00@gmail.com>
2025-03-29 18:44:28 -0500

    refactor: move LoginForm to a new namespace and add Livewire dependency

--- app/Livewire/Auth/LoginForm.php
+++ app/Livewire/Auth/LoginForm.php
@@ -1,7 +1,8 @@
-namespace App\Http\Livewire\Auth;
+<?php
+
+namespace App\Livewire\Auth;
 
 use Livewire\Component;
-use Illuminate\Support\Facades\Auth;
 
 class LoginForm extends Component
 {
@@ -20,7 +21,6 @@ class LoginForm extends Component
             session()->flash('error', 'Credenciales incorrectas');
         }
     }
-
     public function render()
     {
         return view('livewire.auth.login-form');
